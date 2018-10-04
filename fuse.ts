707eb187db312d7e16b32e71a122887daff5147d
vutr <me@vutr.io>
2018-10-04 20:39:01 +0700

    Change docker image

--- fuse.ts
+++ fuse.ts
@@ -4,7 +4,8 @@ import { BackendConfig, FrontendConfig } from './config'
 
 const envVars = {
   NODE_ENV: 'development',
-  PORT: 3000
+  PORT: 3000,
+  DB: 'mongodb://root:1234@localhost:27017/dev'
 }
 
 const backendServe = () => {
