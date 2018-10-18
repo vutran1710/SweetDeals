eb99d2059c870278d7545e526067fb9556b81664
vutr <me@vutr.io>
2018-10-18 20:51:19 +0700

    setup coverage

---
+++ server/test/setup.js
@@ -0,0 +1,7 @@
+const tsConfig = require('./tsconfig.json')
+const tsConfigPaths = require('tsconfig-paths')
+const baseUrl = './'
+tsConfigPaths.register({
+  baseUrl,
+  paths: tsConfig.compilerOptions.paths
+})
