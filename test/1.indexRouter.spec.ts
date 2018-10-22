7076b66f7ab784f387405dcdc606e6f6857de33b
vutr <me@vutr.io>
2018-10-22 15:52:56 +0700

    Add more test

--- test/1.indexRouter.spec.ts
+++ test/1.indexRouter.spec.ts
@@ -5,7 +5,7 @@ import { expect } from 'chai'
 import app from '../server/index'
 
 describe('## 1. Get index', () => {
-  it('should return 200 with a html', done => {
+  it('1.1 Should return 200 with a html', done => {
     request(app)
       .get('/')
       .expect(200)
@@ -13,5 +13,14 @@ describe('## 1. Get index', () => {
         expect(res.text).to.be.a('string')
         done()
       })
+  })
+
+  it('1.2 Should return 500 when error happens', done => {
+    // Fake failure to load react at server side
+
+    request(app)
+      .get('/')
+      .expect(500)
+      .end(done)
   })
 })
