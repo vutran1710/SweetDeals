d691c01f856be6ef0ebe56c7bcde0a2f1d179ff8
vutran1710 <me@vutr.io>
2019-07-13 22:05:05 +0700

    fix

--- shellscript.sh
+++ shellscript.sh
@@ -4,10 +4,12 @@ PATH=./node_modules/.bin:$PATH
 function precommit {
     current_branch=$(git rev-parse --abbrev-ref HEAD)
 
-    echo "<<$current_branch>>"
+    echo "==== ACTIVE BRANCH: [$current_branch]"
 
     if [ "$current_branch" == "master" ]; then
-       git checkout -b "WIP_$USER__$(date)"
+        user=$USER
+        now=$(date+'%m%d-%H%M')
+        git checkout -b "WIP_$user__$now"
     else
         echo "Not in master"
     fi
