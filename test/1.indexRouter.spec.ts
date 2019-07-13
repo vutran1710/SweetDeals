639d44776fb6909bb16d49208b8c1751285b14c5
vutran1710 <me@vutr.io>
2019-07-14 04:28:48 +0700

    fix all

--- test/1.indexRouter.spec.ts
+++ test/1.indexRouter.spec.ts
@@ -5,23 +5,23 @@ import { expect } from 'chai'
 import app from '../server/index'
 
 describe('## 1. Get index', () => {
+  const agent = request.agent(app)
+
   it('1.1 Should return 200 with a html', done => {
-    request(app)
-      .get('/')
-      .expect(200)
-      .end((err, res) => {
-        expect(res.text).to.be.a('string')
-        done()
-      })
+    agent.get('/')
+         .expect(200)
+         .end((err, res) => {
+           expect(res.text).to.be.a('string')
+           done()
+         })
   })
 
   it('1.2 Should return 404 Page when unknown route requested', done => {
-    request(app)
-      .get('/sdfsdfs')
-      .expect(200)
-      .end((err, res) => {
-        expect(res.text).to.contain('404')
-        done()
-      })
+    agent.get('/sdfsdfs')
+         .expect(200)
+         .end((err, res) => {
+           expect(res.text).to.contain('404')
+           done()
+         })
   })
 })
