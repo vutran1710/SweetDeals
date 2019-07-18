e630cc54081fa952bad24f9f3f8304ea7915ca30
vutran1710 <me@vutr.io>
2019-07-18 15:37:04 +0700

    auto publish image & deploy

--- shellscript.sh
+++ shellscript.sh
@@ -15,4 +15,20 @@ function precommit {
     fi
 }
 
+function get_version {
+    PACKAGE_VERSION=$(node -pe "require('./package.json').version")
+    echo $PACKAGE_VERSION
+}
+
+function publish_new_image {
+    version=$(get_version)
+    docker build -t vutrio/sweetdeals-image-demo:$version .
+    docker push vutrio/sweetdeals-image-demo:$version
+}
+
+function roll_update {
+    version=$(get_version)
+    kubectl rolling-update app-controller --image=vutrio/sweetdeals-image-demo:$version
+}
+
 "$@"
