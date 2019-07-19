19a7955476c9dce38e93eba9c9f4c144f9cf8b63
vutran1710 <me@vutr.io>
2019-07-19 10:50:37 +0700

    fix

--- build.sh
+++ build.sh
@@ -1,24 +1,16 @@
 # !/bin/sh
-PATH=./node_modules/.bin:$PATH
+PACKAGE_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g'
+  | tr -d '[[:space:]]')
 
-get_version() {
-    PACKAGE_VERSION=$(node -pe "require('./package.json').version")
-    echo $PACKAGE_VERSION
-}
-
-publish_new_image() {
-    echo "PUBLISHING NEW APP IMAGE >>>"
-    version=$(get_version)
-    echo "- version: $version"
-    echo "- build: docker build -t vutrio/sweetdeals-image-demo:$version ."
-    docker build -t vutrio/sweetdeals-image-demo:$version .
-    echo "- publish: docker push vutrio/sweetdeals-image-demo:$version"
-    docker push vutrio/sweetdeals-image-demo:$version
-}
-
-roll_update() {
-    version=$(get_version)
-    kubectl rollout app-controller --image=vutrio/sweetdeals-image-demo:$version
-}
-
-"$@"
+echo "PUBLISHING NEW APP IMAGE >>>"
+version=$(get_version)
+echo "- version: $version"
+echo "- build: docker build -t vutrio/sweetdeals-image-demo:$version ."
+docker build -t vutrio/sweetdeals-image-demo:$version .
+echo "- publish: docker push vutrio/sweetdeals-image-demo:$version"
+docker push vutrio/sweetdeals-image-demo:$version
+kubectl rollout app-controller --image=vutrio/sweetdeals-image-demo:$version
