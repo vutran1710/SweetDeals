559444fa81e55c18b6dd150dc751ceaffb87d46b
vutran1710 <me@vutr.io>
2019-07-19 11:05:22 +0700

    fix

--- buildscript.sh
+++ buildscript.sh
@@ -2,11 +2,12 @@
 
 PACKAGE_VERSION="$(node -pe \"require('./package.json').version\")"
 
-echo "PUBLISHING NEW APP IMAGE >>>"
-version=$(get_version)
+echo "PUBLISHING NEW APP IMAGE...."
+version="$(get_version)"
 echo "- version: $version"
 echo "- build: docker build -t vutrio/sweetdeals-image-demo:$version ."
 docker build -t vutrio/sweetdeals-image-demo:$version .
 echo "- publish: docker push vutrio/sweetdeals-image-demo:$version"
 docker push vutrio/sweetdeals-image-demo:$version
+echo "- update: kubectl rollout app-controller --image=vutrio/sweetdeals-image-demo:$version"
 kubectl rollout app-controller --image=vutrio/sweetdeals-image-demo:$version
