b462bfb8d453e9a0dcb792b8ce8b86e8d71a024f
vutran1710 <me@vutr.io>
2019-08-03 23:57:44 +0700

    fix path

--- server/routes/indexRouter.tsx
+++ server/routes/indexRouter.tsx
@@ -2,13 +2,14 @@ import { LandingPage } from 'client/LandingPage'
 import * as template from 'es6-template-strings'
 import * as express from 'express'
 import { readFile as fsReadFile } from 'fs'
+import * as path from 'path'
 import * as React from 'react'
 import * as ReactDOMServer from 'react-dom/server'
 import { StaticRouter } from 'react-router-dom'
 import { promisify } from 'util'
 
 const router = express.Router()
-const htmlPath = process.env.HTML_PATH
+const htmlPath = path.resolve(process.env.HTML_PATH)
 const readFile = promisify(fsReadFile)
 
 const jsx = (context, req) => (
